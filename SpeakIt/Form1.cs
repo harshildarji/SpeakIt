d6d768530ee8e21fa0df91209367e4ee227cb3df
Harshil Darji <darjiharshil2994@gmail.com>
2016-06-09 17:16:04 +0530

    Initial commit

---
+++ SpeakIt/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpeakIt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            SpeechSynthesizer speech = new SpeechSynthesizer();
+            speech.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult);
+            speech.Volume = 100;
+            speech.Speak(text);
+        }
+    }
+}
